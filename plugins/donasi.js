7b48c7cdd84f3de123a2ec68a73831e88f8ff8a9
Azimafauzan <102046924+Azimafauzan@users.noreply.github.com>
2022-03-21 23:59:30 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,17 +1,11 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => await conn.sendButtonLoc(m.chat, await (await fetch(fla + 'donasi')).buffer(), `
 ┌〔 Donasi • support 〕
-├ cukup gunakan saia dengan 
-├ bijak 😀
-├
-├ https://saweria.co/Doraemond
-├
-├ saya harap kamu menyukai bot 
-├ saya dan jangan lupa untuk 
-├ mensupport saya karena saya 
-├ sangat menyukai Dora :D
+├ Pulsa : 082259123873
+├ Shopeepay : 082259123873
+├ Saweria : saweria.co/mursid25
 └────
-`.trim(), '© Doraemond', 'Donasi', '.donasi', m)
+`.trim(), '© LiaBot', 'Owner Bot', '.owner', m)
 handler.help = ['donasi']
 handler.tags = ['info']
 handler.command = /^dona(te|si)$/i
